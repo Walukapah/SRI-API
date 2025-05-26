610055e25d3148bc9f5be2a42e49aa58000ed058
Walukapah <90122948+Walukapah@users.noreply.github.com>
2025-05-26 09:39:33 +0530

    Update youtubedl.js

--- api/youtubedl.js
+++ api/youtubedl.js
@@ -4,10 +4,10 @@ const { URL } = require('url');
 
 // Helper functions
 const formatDuration = (duration) => {
-  const match = duration.match(/PT(\d+H)?(\d+M)?(\d+S)?/);
+  const match = duration.match(/PT(?:(\d+)H)?(?:(\d+)M)?(?:(\d+)S)?/);
   const hours = (parseInt(match[1]) || 0);
   const minutes = (parseInt(match[2]) || 0);
-  const seconds = (parseInt(match[3]) || 0;
+  const seconds = (parseInt(match[3]) || 0);
   
   return [
     hours.toString().padStart(2, '0'),
