1b7923bcb6a9ac4c84190e2303048fe91d8c5a53
Walukapah <90122948+Walukapah@users.noreply.github.com>
2025-06-21 20:53:21 +0530

    Update pronhubdl.js

--- api/pronhubdl.js
+++ api/pronhubdl.js
@@ -9,22 +9,7 @@ module.exports = async (url) => {
 
   try {
     const video = await pornhub.video(url);
-    return {
-      title: video.title,
-      url: video.url,
-      duration: video.duration,
-      views: video.views,
-      rating: video.rating,
-      percent: video.percent,
-      tags: video.tags.map(tag => tag.name),
-      thumbnails: video.preview,
-      author: {
-        name: video.author?.name,
-        url: video.author?.url,
-        verified: video.author?.verified,
-      },
-      download: video.downloadUrls || []
-    };
+    res.json(video)
   } catch (error) {
     throw new Error(error.message);
   }
