80639c0385ec3791d5de26ec2fb035096541f2c9
Walukapah <90122948+Walukapah@users.noreply.github.com>
2025-06-21 20:58:16 +0530

    Update pronhubdl.js

--- api/pronhubdl.js
+++ api/pronhubdl.js
@@ -8,9 +8,9 @@ module.exports = async (url) => {
   }
 
   try {
-    const video = await pornhub.video(url);
+    const video = await pornhub.video(url)
     res.json(video)
-  } catch (error) {
-    throw new Error(error.message);
+  } catch (err) {
+    res.status(500).json({ error: 'Failed to fetch video info', details: err.message })
   }
 };
